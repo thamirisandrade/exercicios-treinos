3c5f1ade966d87a02de4367df8ed672c877da8c2
Thamiris Andrade <thamirisfandrade@hotmail.com>
2025-07-11 12:47:53 -0300

    Update exercicios_for.py
    removi o emogi para ficar melhor

--- exercicios_for.py
+++ exercicios_for.py
@@ -3,15 +3,15 @@ soma_impares = 0
 for numero in range(1, 11):
     if numero % 2 != 0:
         soma_impares += numero
-print(f"\n📘 Soma dos números ímpares de 1 a 10: {soma_impares}")
+print(f"\n Soma dos números ímpares de 1 a 10: {soma_impares}")
 
 # Ordem decrescente
-print("\n📘 Números de 10 a 1 em ordem decrescente:")
+print("\n Números de 10 a 1 em ordem decrescente:")
 for numero in range(10, 0, -1):
     print(numero)
 
 # Tabuada de um número com input
-print("\n📘 TABUADA DE UM NÚMERO")
+print("\n TABUADA DE UM NÚMERO")
 try:
     numero = int(input("Digite um número para ver a tabuada de 1 a 10: "))
     print(f"\nTabuada de {numero}:")
@@ -21,7 +21,7 @@ except ValueError:
     print("❌ Entrada inválida! Por favor, digite um número inteiro.")
 
 # Soma de elementos de uma lista
-print("\n📘 SOMANDO ELEMENTOS DE UMA LISTA")
+print("\n SOMANDO ELEMENTOS DE UMA LISTA")
 numeros = [3, 5, 7, 9, 11]
 try:
     soma_total = sum(numeros)
@@ -31,7 +31,7 @@ except Exception as erro:
     print("❌ Ocorreu um erro ao somar os números:", erro)
 
 # Média de elementos de uma lista
-print("\n📘 MÉDIA DOS ELEMENTOS DA LISTA")
+print("\nMÉDIA DOS ELEMENTOS DA LISTA")
 valores = [10, 20, 30, 40, 50]  # Testa depois com lista vazia também
 
 try:
